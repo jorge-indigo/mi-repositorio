9111fda0d57b23e3e7d08b441b8495691f931019
Jorge Eduardo <jorger@dsindigo.mx>
2017-08-12 12:58:22 -0500

    Eliminando los archivos de David >:V

--- dia2/ejercicio_elementosFaltantes.js
+++
@@ -1,7 +0,0 @@
-var x = [3, 4, 5, 8, 10, 15];
-
-function numbers(xx){
-  return (Math.max.apply( Math, xx ) - Math.min.apply( Math, xx ) - xx.length + 1);
-}
-
-console.log("Faltantes =",numbers(x));
